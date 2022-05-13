2d3fac7d33fbb34740d500c36fefd0bb34cea782
rungene <rungenesamuel@gmail.com>
2022-05-13 05:26:14 +0300

    refactor: creating mRecylerview private var

--- app/src/main/java/com/rungenes/firebasevisualsapplication/HomeFragment.kt
+++ app/src/main/java/com/rungenes/firebasevisualsapplication/HomeFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 
 class HomeFragment : Fragment() {
 
+    private var mRecyclerview: RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +23,6 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
 
