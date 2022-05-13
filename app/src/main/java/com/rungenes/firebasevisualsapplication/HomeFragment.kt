dcc791c7cce1511647c4ecc541eaeecd886676cf
rungene <rungenesamuel@gmail.com>
2022-05-13 05:10:34 +0300

    refactor: override onViewCreated function

--- app/src/main/java/com/rungenes/firebasevisualsapplication/HomeFragment.kt
+++ app/src/main/java/com/rungenes/firebasevisualsapplication/HomeFragment.kt
@@ -19,5 +19,10 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
 
 }
