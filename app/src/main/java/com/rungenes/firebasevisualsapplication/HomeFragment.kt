6689f182d8bab4d3e369b60e6b187a3d7fe703df
rungene <rungenesamuel@gmail.com>
2022-05-25 05:15:17 +0300

    refactor : Adding onCreateOptionsMenu()

--- app/src/main/java/com/rungenes/firebasevisualsapplication/HomeFragment.kt
+++ app/src/main/java/com/rungenes/firebasevisualsapplication/HomeFragment.kt
@@ -4,10 +4,8 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -371,6 +369,10 @@ class HomeFragment : Fragment() {
         if (firebaseRecyclerAdapter != null) {
             firebaseRecyclerAdapter!!.stopListening()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 
